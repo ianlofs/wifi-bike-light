cb6b15402e056425d6bb3d4e143ac6da2cee08d8
ian lofgren <i.lof33@gmail.com>
2016-12-12 20:13:39 -0800

    more stuff

--- upload.js
+++ upload.js
@@ -1,24 +1,50 @@
-var fileReader = new FileReader()
+var img = new Image()
+
 
 $('.file-input').change(function(){
   var file = this.files[0];
-  var name = file.name;
-  var size = file.size;
-  var type = file.type;
-  console.log(file);
-  previewFile(file)
-});
-
-function previewFile(file) {
-  var preview = document.querySelector('#file-preview');
-  console.log()
   var reader = new FileReader();
-
+  var canvas = $('#file-preview')[0];
+  var ctx = canvas.getContext('2d');
   reader.onload = function () {
-    preview.src  = reader.result;
-  }
+    img.src = reader.result;
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
 
-  if (file) {
-    reader.readAsDataURL(file);
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imageData.data;
+
+    var imageArray = new Array(img.height);
+    for (var i = 0; i < imageArray.length; i++) {
+      imageArray[i] = new Array(img.width);
+      for (var j = 0; j < imageArray[i].length; j++) {
+        imageArray[i][j] = {};
+      }
+    }
+
+    imageArray.map(function(array, i) {
+      return array.map(function(pixelData, j) {
+        var pixelPos = (i * img.width * 4) + (j * 4);
+        pixelData.red = data[pixelPos];
+        pixelData.green = data[pixelPos+1];
+        pixelData.blue = data[pixelPos+2];
+        pixelData.alpha = data[pixelPos+3];
+        return pixelData;
+      });
+    })
+
+    for(var i = 0; i < img.height; i++){
+      for(var j = 0; j < img.width; j++){
+        var pixelPos = (i * img.width * 4) + (j * 4);
+        data[pixelPos] = imageArray[i][j].red;
+        data[pixelPos + 1] = imageArray[i][j].green;
+        data[pixelPos + 2] = imageArray[i][j].blue;
+        data[pixelPos + 3] = imageArray[i][j].alpha;
+      }
+    }
+    imageData.data = data;
+    ctx.putImageData(imageData, 0, 0);
   }
-}
+  reader.readAsDataURL(file);
+});
